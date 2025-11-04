5a91a7b27617c62f6ee549b9a712c1c51c265475
felix <larosefelixb@gmail.com>
2025-11-04 15:30:09 -0500

    def de class modele carree rectangle border

--- jeu.py
+++ jeu.py
@@ -13,7 +13,31 @@ class Modele():
 
 class Carree():
     def __init__(self):
-        pass
+        self.dimension
+        self.posX
+        self.posY
+        self.couleur = "rouge"
+        self.vitesse
+    
+
+class Rectangle():
+    def __init__(self):
+        self.largeur
+        self.hauteur
+        self.posX
+        self.posY
+        self.couleur = "bleue"
+        self.vitesse
+
+class Border():
+    def __init__(self):
+        self.largeur
+        self.hauteur
+        self.posX
+        self.posY
+        self.couleur = "noire"
+
+
 
 class Vue():
     def __init__(self, parent, largeur, hauteur):         # c'est une convention de dire self mais c'est une variable comme une autre
