253891314a80e24d1aa5a31ec05d4cc0147dd081
felix <larosefelixb@gmail.com>
2025-11-04 16:41:09 -0500

    modele continué

--- jeu.py
+++ jeu.py
@@ -8,12 +8,42 @@ class Balloune():
         self.pos_y = pos_y
 
 class Modele():
-    def __init__(self):
-        pass
+    def __init__(self, parent, largeur, hauteur):
+        self.parent = parent
+        self.largeur = largeur
+        self.hauteur = hauteur
+        self.carree = Carree(self, 40, 255, 255)
+        self.rectangle1 = Rectangle(self, 60, 60, 100, 100)
+        self.rectangle2 = Rectangle(self, 60, 50, 300, 85)
+        self.rectangle3 = Rectangle(self, 30, 60, 85, 350)
+        self.rectangle4 = Rectangle(self, 100, 20, 355, 340)
 
 class Carree():
-    def __init__(self):
-        pass
+    def __init__(self, parent, dimension, posX, posY):
+        self.parent = parent
+        self.dimension = dimension
+        self.posX = posX
+        self.posY = posY
+        self.couleur = "rouge"
+
+class Rectangle():
+    def __init__(self, parent, largeur, hauteur, posX, posY, vitesse):
+        self.parent = parent
+        self.largeur = largeur
+        self.hauteur = hauteur
+        self.posX = posX
+        self.posY = posY
+        self.couleur = "bleue"
+        self.vitesse = vitesse
+
+class Border():
+    def __init__(self, parent, largeur, hauteur, posX, posY):
+        self.parent = parent
+        self.largeur = largeur
+        self.hauteur = hauteur
+        self.posX = posX
+        self.posY = posY
+        self.couleur = "noire"
 
 class Vue():
     def __init__(self, parent, largeur, hauteur):         # c'est une convention de dire self mais c'est une variable comme une autre
